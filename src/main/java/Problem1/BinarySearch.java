7dc94d5aa56772532a1b3bb63969c53f19df8f9e
Danny Kha <danny.n.kha@gmail.com>
2020-10-31 22:17:49 -0700

    coolio

--- src/main/java/Problem1/BinarySearch.java
+++ src/main/java/Problem1/BinarySearch.java
@@ -5,7 +5,20 @@ public class BinarySearch {
     // Do not change signature (return type, function name, parameter types)
     // Add a helper function for recursion calls
     public static int binarySearch(int[] data, int target) {
-        // homework
-        return -1;  // place holder
+        return binarySearch(data, target, 0, data.length -1);  // place holder
+    }
+
+    private static int binarySearch(int[] data, int target, int left, int right) {
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            if (target < data[middle]) {
+                right = middle - 1;
+            } else if (target > data[middle]) {
+                left = middle + 1;
+            } else {
+                return middle;
+            }
+        }
+        return -1;
     }
 }
--- src/main/java/Problem1/ReverseStack.java
+++ src/main/java/Problem1/ReverseStack.java
@@ -2,8 +2,23 @@ package Problem1;
 
 import java.util.Stack;
 
+// pringles can is stack, take off all the tops and then put them back in
+
 public class ReverseStack {
     public static void reverseStack(Stack<Integer> stack) {
-        // homework
+        if(!stack.isEmpty()) { // SIMPLIFIED
+            int x = stack.pop();
+            reverseStack(stack);
+            bottomStack(stack, x);
+        }
+    }
+    public static void bottomStack(Stack<Integer> stack, int x) {
+        if (stack.isEmpty()) { // wait wot
+            stack.push(x);
+            return;
+        }
+        int y = stack.pop();
+        bottomStack(stack, x);
+        stack.push(y);
     }
 }

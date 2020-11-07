33432636c9c55549220e7ea6a10f77525a11c622
Danny Kha <danny.n.kha@gmail.com>
2020-11-06 22:49:59 -0800

    sort of fun, big learning curve

--- src/main/java/Problem2/SingleLinkedList.java
+++ src/main/java/Problem2/SingleLinkedList.java
@@ -67,7 +67,6 @@ public class SingleLinkedList {
         return size;
     }
 
-    // reverse the linked list RECURSIVELY
     public void reverse() {
         if (head == null) {
             return;

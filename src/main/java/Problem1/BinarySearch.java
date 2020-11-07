689496a05b8f83c567d3de2af642fb4465e6709e
Danny Kha <danny.n.kha@gmail.com>
2020-11-06 22:47:46 -0800

    sort of fun, big learning curve

--- src/main/java/Problem1/BinarySearch.java
+++ src/main/java/Problem1/BinarySearch.java
@@ -1,23 +1,22 @@
 package Problem1;
 
 public class BinarySearch {
-    // This is to be done recursively
-    // Do not change signature (return type, function name, parameter types)
-    // Add a helper function for recursion calls
     public static int binarySearch(int[] data, int target) {
-        return binarySearch(data, target, 0, data.length -1);  // place holder
+        return binarySearch(data, target, 0, data.length - 1);
     }
-
     private static int binarySearch(int[] data, int target, int left, int right) {
-        while (left <= right) {
-            int middle = left + (right - left) / 2;
-            if (target < data[middle]) {
-                right = middle - 1;
-            } else if (target > data[middle]) {
-                left = middle + 1;
-            } else {
-                return middle;
-            }
+        int middle = left + (right - left) / 2;
+        if (right < left) {
+            return -1;
+        }
+        if (target < data[middle]) {
+            return binarySearch(data, target, left, middle - 1);
+        }
+        if (target > data[middle]) {
+            return binarySearch(data, target, middle +1, right);
+        }
+        if (target == data[middle]) {
+            return middle;
         }
         return -1;
     }
--- src/main/java/Problem2/SingleLinkedList.java
+++ src/main/java/Problem2/SingleLinkedList.java
@@ -69,6 +69,24 @@ public class SingleLinkedList {
 
     // reverse the linked list RECURSIVELY
     public void reverse() {
-        // homework
+        if (head == null) {
+            return;
+        }
+        head.next = reverse(head.next);
+    }
+
+    public ListNode reverse(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        if (head.next == null) {
+            return head;
+        }
+        ListNode newNode = reverse(head.next);
+
+        head.next.next = head;
+        head.next = null;
+
+        return newNode;
     }
 }
